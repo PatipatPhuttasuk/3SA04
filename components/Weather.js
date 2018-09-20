a1765b18b7bbf505dc5ed4745e7421ca9cfdbb4f
PatipatPhuttasuk <5835512104@psu.ac.th>
2018-09-20 12:36:06 +0700

    first commit

---
+++ components/Weather.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import Forcast from './Forcast'
+export default class Weather extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            forcast: {
+                main: '-', description: '-', temp: 0
+            }
+        }
+    }
+    render() {
+        return (
+            <View style={styles.container}>
+                <ImageBackground source={require('../bg.jpeg')} style={styles.backdrop}>
+                    <Text>Zip code is {this.props.zipCode}.</Text>
+                    <Forcast {...this.state.forcast} />
+                </ImageBackground>
+            </View>
+        );
+    }
+}
+const styles = StyleSheet.create({
+    container: { paddingTop: 25 },
+    backdrop: { width: '100%', height: '100%' },
+});
