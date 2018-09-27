dccb73aa2e419650176afed31c933e29d832f2bd
PatipatPhuttasuk <5835512104@psu.ac.th>
2018-09-27 13:25:55 +0700

    first commit

--- components/Weather.js
+++ components/Weather.js
@@ -1,27 +1,85 @@
-import React from 'react';
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
-import Forcast from './Forcast'
+import React from "react";
+import { StyleSheet, View, Text, ImageBackground } from "react-native";
+import Forecast from "./Forecast";
+
 export default class Weather extends React.Component {
     constructor(props) {
         super(props);
+        this.APPID = "886705b4c1182eb1c69f28eb8c520e20";
         this.state = {
-            forcast: {
-                main: '-', description: '-', temp: 0
+            forecast: {
+                main: "main",
+                description: "description",
+                temp: 0
             }
+        };
+    }
+
+    componentDidMount = () => this.fetchData();
+    componentDidUpdate = (prevProps) => {
+        if (prevProps.zipCode !== this.props.zipCode) {
+            this.fetchData()
         }
     }
+
+
+    fetchData = () => {
+        fetch(
+            `http://api.openweathermap.org/data/2.5/weather?q=${
+            this.props.zipCode
+            },th&units=metric&APPID=${this.APPID}`
+        )
+            .then(response => {
+                return response.json();
+            })
+            .then(json => {
+                this.setState({
+                    forecast: {
+                        main: json.weather[0].main,
+                        description: json.weather[0].description,
+                        temp: json.main.temp
+                    }
+                });
+            })
+            .catch(error => {
+                console.warn(error);
+            });
+    };
     render() {
         return (
             <View style={styles.container}>
-                <ImageBackground source={require('../bg.jpeg')} style={styles.backdrop}>
-                    <Text>Zip code is {this.props.zipCode}.</Text>
-                    <Forcast {...this.state.forcast} />
+                <ImageBackground source={require("../bt.jpg")} style={styles.backdrop}>
+                    <View style={styles.app}>
+                        <Text style={styles.font}>Zip code is {this.props.zipCode}.</Text>
+                        <Forecast {...this.state.forecast} />
+                    </View>
+                    <Text style={styles.black}> </Text>
+                    <Text style={styles.black}> </Text>
+                    <Text style={styles.black}>                 By Patipat Phuttasuk</Text>
+                    <Text style={styles.black}>                  ID :   5835512104</Text>
                 </ImageBackground>
             </View>
         );
     }
 }
+
 const styles = StyleSheet.create({
     container: { paddingTop: 25 },
-    backdrop: { width: '100%', height: '100%' },
+    backdrop: { width: "100%", height: "100%" },
+    app: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        backgroundColor: "rgba(0,0,0,0.8)"
+    },
+    font: {
+        color: "white",
+        paddingTop: 25,
+        fontSize: 18
+    },
+    black: {
+        color: "black",
+        paddingTop: 35,
+        fontSize: 18
+    }
 });
